c907456172f8e393e3ac2dd2c66ea0dae0e459b2
lisaconnect1234 <lisaconnect1234@gmail.com>
2020-09-19 23:49:19 -0600

    New python files

---
+++ src/haversine.py
@@ -0,0 +1,20 @@
+from math import radians, cos, sin, asin, sqrt
+#Compute the Haversine distance between samples in X and Y
+#The Haversine (or great circle) distance is the angular distance between two points on the surface of a sphere. 
+#The first distance of each point is assumed to be the latitude, the second is the longitude, given in radians.
+
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance between two points 
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians 
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula 
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a)) 
+    r = 6371 # Radius of earth in kilometers. Use 3956 for miles
+    return c * r
