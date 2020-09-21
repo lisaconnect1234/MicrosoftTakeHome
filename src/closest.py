1cfceffeaf32f1e8e39158e0ca39f1e001b1afb4
lisaconnect1234 <lisaconnect1234@gmail.com>
2020-09-21 09:39:18 -0600

    update the cache comments

--- src/closest.py
+++ src/closest.py
@@ -5,7 +5,7 @@ from haversine import *
 
 
 def read_data(cache):
-    #Open the file in read mode that contains the food truck data in the area and load it into a python array 
+    #Open the file in read mode that contains the food truck data in the area and load it into a python array and cache
     with open('SanFranciscoFoodTrucks.json', 'r') as j:
         json_data = json.load(j)
         cache.set("data",json_data)
@@ -33,14 +33,14 @@ def findClosest(inLat,inLon,numTrucks,cache):
     #Store the distance in the array, so the closest is at the top
     distList.sort(key=sortDist)
     
-    #Add the 5 closest food trucks to an array
+    #Add the numTrucks closest food trucks to an array
     x = 0
     closest = [];
     while (x < numTrucks):
         closest.append(distList[x])
         x += 1
        
-    #Return the 5 closest food trucks in the json array    
+    #Return the numTrucks closest food trucks in the json array    
     return json.dumps(closest)
 
 
